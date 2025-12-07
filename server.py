42bfc2845d322b9ddf10c5b275bf23a11e1c0fb0
Yu-26 <kurii.vgan25@gmail.com>
2025-12-07 10:48:36 +0900

    Add functionality to create credentials.json
    Create credentials.json from environment variable if it exists.

--- server.py
+++ server.py
@@ -8,3 +8,11 @@ def home():
 
 if __name__ == "__main__":
     app.run(port=5000)
+
+import os
+
+# Render の環境変数から credentials.json を作成
+if "GCP_CREDENTIALS_JSON" in os.environ:
+    with open("credentials.json", "w", encoding="utf-8") as f:
+        f.write(os.environ["GCP_CREDENTIALS_JSON"])
+
